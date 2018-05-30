de8e8ea10476b94a35e98e3da42a30dd3c8007fd
Björn Bergdagl <molnet@gmail.com>
2018-05-30 10:10:18 +0200

    Added answer for aug16no1 again

---
+++ src/aug16no1/aug16no1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aug16no1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Björn
+ */
+public class aug16no1 {
+
+
+
+
+
+    static int numberOfNumbers;
+    static int[] talen = new int[numberOfNumbers];
+    
+    public static void main(String[] args){
+        numberOfNumbers = inputSize();
+        talen = inputNumbers();
+        printNumbers();
+    }
+    
+    private static int inputSize() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ange antal siffror att mata in: ");
+        int inSize = input.nextInt();
+        return inSize;
+    }
+        
+    private static int[] inputNumbers() {
+        
+        int[] temp = new int[numberOfNumbers];
+        Scanner input = new Scanner(System.in);
+        for (int i=0; i < numberOfNumbers; i++){   
+            System.out.println("Ange dina siffror ("+(numberOfNumbers-i)+" kvar): ");
+            temp[i]= input.nextInt();
+        }
+        return temp;   
+    }
+    
+    private static void printNumbers() {
+        System.out.println("Talserien baklänges");
+        for (int i = (numberOfNumbers-1); i>=0 ; i-- )  {
+            System.out.println(talen[i]);
+        }
+    }
+
+}
